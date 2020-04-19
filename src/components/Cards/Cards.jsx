8d902f0ac5f563e7da240476940709476f4fcd41
weilyuwang <weilyuwang@gmail.com>
2020-04-18 21:55:28 -0400

    add two more cards - hard coded for now

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -19,6 +19,32 @@ const Cards = (props) => {
                         </Typography>
                     </CardContent>
                 </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Recovered
+                        </Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">
+                            Number of recoveries from COVID-19
+                        </Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Deaths
+                        </Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">
+                            Number of deaths caused by COVID-19
+                        </Typography>
+                    </CardContent>
+                </Grid>
             </Grid>
         </div>
     );
