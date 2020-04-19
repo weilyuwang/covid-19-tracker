2025959dd9ce1b5bf89d7e8ff01ef94595b8c640
weilyuwang <weilyuwang@gmail.com>
2020-04-18 22:35:41 -0400

    add chart.js dependency

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+
+import styles from "./Chart.module.css";
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            const dailyData = await fetchDailyData();
+            setDailyData(dailyData);
+        };
+
+        fetchAPI();
+    });
+
+    const lineChart = dailyData[0] ? (
+        <Line
+            data={{
+                labels: dailyData(({ date }) => date),
+                datasets: [{}, {}],
+            }}
+        />
+    ) : null;
+
     return <h1>Chart</h1>;
 };
 
